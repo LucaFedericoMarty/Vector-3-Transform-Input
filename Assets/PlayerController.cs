611d2c54bf3c3625e3c196ddad5b46a6e079af2b
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-05-17 14:29:22 -0300

    Clase Jero Vector 3 y Movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(movementSpeed, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-movementSpeed, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, movementSpeed, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -movementSpeed, 0);
+        }
+    }
+}
